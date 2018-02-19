b886ab51fe077ef0b9e198868b57d2d227d0140d
Paweł Popowicz <p.p.popowicz@gmail.com>
2018-02-19 22:20:50 +0100

    skeleton of application inited

---
+++ gui.py
@@ -0,0 +1,14 @@
+def print_program_menu(menu_commands):
+"""
+arg : list of commends
+function displays grafical menu
+"""
+pass
+
+def choose_option(input_numbers):
+
+pass
+
+
+
+
---
+++ main.py
@@ -0,0 +1,6 @@
+import gui
+import data_operations
+
+
+def main():
+    print_program_menu(menu_commands)
